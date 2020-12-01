77e9f7024031012acf9676bbfabf5b39e3faaa62
kdhaz <a1s337887@gmail.com>
2020-12-01 20:49:57 +0900

    미리보기랑 카메라 각도 조절

--- app/src/main/java/com/samguk/zoom/MainActivity.java
+++ app/src/main/java/com/samguk/zoom/MainActivity.java
@@ -11,52 +11,51 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
 
 import com.samguk.zoom.features.camera.CameraManager;
+import com.samguk.zoom.features.camera.CameraPreview;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CAMERA = 100001;
+    private CameraPreview cameraPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //사용 가능할떄
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            if(checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
-            {
-                requestPermissions(new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CAMERA);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
                 return;
             }
         }
-
-        //사용 불가능할때
         CameraManager manager = CameraManager.getCameraManager();
-        if (!manager.checkCameraUsable(this)) { //사용 가능한가?
-            new AlertDialog.Builder( this)
-                    .setMessage("카메라가 사용 안된다구~~.")
-                    .setNeutralButton( "종료", new DialogInterface.OnClickListener() {
-                          @Override
-                          public void onClick(DialogInterface dialog, int which) {
-                                System.exit( 0);
-                         }
-                     })
-                     .show();
-
+        if (!manager.checkCameraUsable(this)) {
+            new AlertDialog.Builder(this)
+                    .setMessage("카메라가 사용 불가합니다")
+                    .setNeutralButton("종료", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .show();
         }
-
+        Camera camera = manager.getCamera();
+        cameraPreview = new CameraPreview(this, camera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(cameraPreview);
     }
 
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        switch (requestCode)
-        {
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
             case PERMISSION_REQUEST_CAMERA:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     recreate();
-                }
-                else {
+                } else {
                     finish();
                 }
                 break;
--- app/src/main/java/com/samguk/zoom/features/camera/CameraManager.java
+++ app/src/main/java/com/samguk/zoom/features/camera/CameraManager.java
@@ -8,52 +8,39 @@ import android.util.Log;
 public class CameraManager {
     private static CameraManager cameraManager;
 
-    private CameraManager() {}
+    private CameraManager() {
+    }
 
-
-    //ㅋㅋ
-    public static CameraManager getCameraManager()
-    {
-        if(cameraManager == null) {
+    public static CameraManager getCameraManager() {
+        if (cameraManager == null) {
             cameraManager = new CameraManager();
         }
         return cameraManager;
+
     }
 
-
-    //EZ
     public boolean checkCameraUsable(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             return true;
         } else {
             return false;
         }
     }
 
-
-    //CUTE
     public Camera getCamera() {
         Camera camera = null;
-
         try {
             camera = Camera.open();
             Camera.Parameters cameraParameters = camera.getParameters();
-            if(cameraParameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            if (cameraParameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
                 cameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
                 camera.setParameters(cameraParameters);
             }
-        }catch (Exception ex) {
-            Log.e("CameraManager",ex.toString());
+
+        } catch (Exception ex) {
+            Log.e("CameraManager", ex.toString());
             System.exit(1);
         }
         return camera;
     }
-
-
-
-
-
 }
-
-
-
